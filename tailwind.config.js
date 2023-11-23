d1e85da942c03d3ca2c48b72401ebf009d3f7203
Kevin Salcedo <alu101426846@ull.edu.es>
2023-11-23 13:07:06 +0000

    feat(login): implement initial version of login feature

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'gray-750': 'rgb(31, 41, 68)',
+        'custom-pastel': '#FFDFD3',
+        'custom-pink': '#f0a6ca',
+      },
+    },
   },
   plugins: [],
 };
